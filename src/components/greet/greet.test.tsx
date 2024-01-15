46967259aeb2af92d26e21333742719370773db2
Brown <memoyebrown@gmail.com>
2024-01-15 16:21:30 +0100

    Grouping Tests

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -6,18 +6,29 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
-test("Greet renders correctly", () => {
-  // create virtual DOM of the component with 'render'
-  render(<Greet />);
-  // check if the text "Hello" is present
-  const textElement = screen.getByText("Hello"); // 'getByText' takes strings and regexes
-  // test assertion with 'expect' from jest
-  expect(textElement).toBeInTheDocument();
-});
+// Group multiple tests with 'describe'
+/**
+ * You can use nesr describe blocks
+ * You can use .skip and .only methods with describe blocks
+ * You can have multiple describe blocks in same file
+ * Test Suite = A file
+ *
+ */
+describe("Greet", () => {
+  test("renders correctly", () => {
+    // create virtual DOM of the component with 'render'
+    render(<Greet />);
+    // check if the text "Hello" is present
+    const textElement = screen.getByText("Hello"); // 'getByText' takes strings and regexes
+    // test assertion with 'expect' from jest
+    expect(textElement).toBeInTheDocument();
+  });
 
-// '.only' makes sure only said test is run on save
-test.only("Greet renders with a name", () => {
-  render(<Greet name={"Brown"} />);
-  const textElement = screen.getByText("Hello Brown");
-  expect(textElement).toBeInTheDocument();
+  // '.only' makes sure only said test is run on save
+  // test.only("Greet renders with a name", () => {
+  test("renders with a name", () => {
+    render(<Greet name={"Brown"} />);
+    const textElement = screen.getByText("Hello Brown");
+    expect(textElement).toBeInTheDocument();
+  });
 });
