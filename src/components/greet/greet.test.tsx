03703741623197226108a07912f1754ee4f8ab27
Brown <memoyebrown@gmail.com>
2024-01-15 15:51:06 +0100

    test-driven development (red-green testing)

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,3 +1,8 @@
+/**
+ * Greet should render the text 'hello'.
+ * If a name is passed into the component, it should render 'hello', followed by the name.
+ */
+
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
@@ -5,7 +10,13 @@ test("Greet renders correctly", () => {
   // create virtual DOM of the component with 'render'
   render(<Greet />);
   // check if the text "Hello" is present
-  const textElement = screen.getByText(/hello/i); // 'getByText' takes strings and regexes
+  const textElement = screen.getByText("Hello"); // 'getByText' takes strings and regexes
   // test assertion with 'expect' from jest
+  expect(textElement).toBeInTheDocument();
+});
+
+test("Greet renders with a name", () => {
+  render(<Greet name={"Brown"} />);
+  const textElement = screen.getByText("Hello Brown");
   expect(textElement).toBeInTheDocument();
 });
