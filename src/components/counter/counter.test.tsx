84f9434d64ab04cd91ab0c597afe5551d6c44cdc
Brown <memoyebrown@gmail.com>
2024-01-21 19:01:22 +0100

    keyboard interactions

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -7,7 +7,7 @@ describe("Counter", () => {
     render(<Counter />);
     const countElement = screen.getByRole("heading");
     expect(countElement).toBeInTheDocument();
-    const incrementButton = screen.getByRole("button");
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
     expect(incrementButton).toBeInTheDocument();
   });
 
@@ -45,6 +45,38 @@ describe("Counter", () => {
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent("2");
   });
+
+  test("renders a count of '10' after clicking the set button", async () => {
+    user.setup();
+    render(<Counter />);
+    const amountInput = screen.getByRole("spinbutton"); // for input of [type = "number"], the role is 'spinButton', not textBox
+
+    // type in a value of '10'
+    await user.type(amountInput, "10");
+    expect(amountInput).toHaveValue(10);
+
+    const setButton = screen.getByRole("button", { name: "Set" });
+    await user.click(setButton);
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("10");
+  });
+
+  test("elements are focussed in the right order", async () => {
+    user.setup();
+    render(<Counter />);
+    const amountInput = screen.getByRole("spinbutton");
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+    const setButton = screen.getByRole("button", { name: "Set" });
+
+    await user.tab();
+    expect(incrementButton).toHaveFocus();
+
+    await user.tab();
+    expect(amountInput).toHaveFocus();
+
+    await user.tab();
+    expect(setButton).toHaveFocus();
+  });
 });
 
 /**Convenience APIs
@@ -53,4 +85,27 @@ describe("Counter", () => {
  * - tripleClick()
  * - hover()
  * - unhover()
+ */
+
+/** Convenience API
+ * - tab()
+ */
+
+/** Utility APIs
+ * - clear() - clear editable elements
+ * - selectOptions() - select items in a select dropdown or listbox where you can select multiple elements
+ * - deselectOptions()
+ * - upload()
+ * - type() -
+ */
+
+/**Clipboard APIs
+ * - copy()
+ * - cut()
+ * - paste()
+ */
+
+/**Keyboard APIs
+ * - keyboard('foo') // translates to: f, o, o
+ * - keyboard('{shift>}A{/Shift}') // translates to: Shift(down), A, Shift(up)
  */
