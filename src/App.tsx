542777848d1c2a77eb6467365a6aa325fb4b5f84
Brown <memoyebrown@gmail.com>
2024-01-21 19:27:44 +0100

    Providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import "./App.css";
+import { MuiMode } from "./components/mui/mui-mode";
 import { Skills } from "./components/skills/skills";
+import { AppProviders } from "./providers/app-providers";
 // import { Application } from "./components/application/application";
 
 function App() {
   // return <Application />;
   {
     return (
-      <div
-        style={{
-          minHeight: "100dvh",
-          display: "flex",
-          placeItems: "center",
-        }}
-      >
-        <Skills skills={["HTML", "CSS"]} />
-      </div>
+      <AppProviders>
+        <div
+          style={{
+            minHeight: "100dvh",
+            display: "flex",
+            placeItems: "center",
+          }}
+        >
+          <Skills skills={["HTML", "CSS"]} />
+        </div>
+        <MuiMode />
+      </AppProviders>
     );
   }
 }
